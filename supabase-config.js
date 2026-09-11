eb5d8ce7dbc0b491b7476fdf2a755da14083cd06
calisirilker1-collab <calisirilker1@gmail.com>
2026-09-12 02:02:30 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,5 +5,5 @@
 
 window.VALERA_SUPABASE = {
   url: 'https://iczidgdfpirtltopvddx.supabase.co',
-  publishableKey: 'sb_publishable_nwUAvBU5k4NS8hB9cmNLHg__I_uNnMk
+  publishableKey: 'sb_publishable_nwUAvBU5k4NS8hB9cmNLHg__I_uNnMk'
 };
